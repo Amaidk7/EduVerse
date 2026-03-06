3842b1868a91781d73d1032b3f6227e823751339
Amaidk7 <amaidkhan515@gmail.com>
2026-03-06 16:13:45 +0530

    loginpage

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "../assets/logo.jpg";
+import google from "../assets/google.jpg";
+import { IoEyeOutline } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const [show, setShow] = useState(false);
+  const navigate = useNavigate();
   return (
-    <div>
-      
+    <div className="bg-[#dddbdb] w-screen h-screen flex items-center justify-center">
+      <form className="w-[90%] md:w-200 h-150 bg-[white] shadow-xl rounded-2xl flex">
+        {/* left div */}
+        <div className="md:w-[50%] w-full h-full flex flex-col items-center justify-center gap-3">
+          <div>
+            <h1 className="font-semibold text-[black] text-2xl">
+              Welcome back
+            </h1>
+            <h2 className="text-[#999797] text-[18px]">Login your account</h2>
+          </div>
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3">
+            <label htmlFor="email" className="font-semibold">
+              Email
+            </label>
+            <input
+              id="email"
+              type="text"
+              className="border w-full h-8.75 border-[#e7e6e6] text-[15px] px-5"
+              placeholder="Your Email"
+            />
+          </div>
+          <div className="flex flex-col gap-1 w-[80%] items-start justify-center px-3 relative">
+            <label htmlFor="password" className="font-semibold">
+              Password
+            </label>
+            <input
+              id="password"
+              type={show ? "text" : "password"}
+              className="border w-full h-8.75 border-[#e7e6e6] text-[15px] px-5"
+              placeholder="Your Password"
+            />
+            {!show ? (
+              <IoEyeOutline
+                className="absolute w-5 h-5 cursor-pointer right-[5%] bottom-[10%]"
+                onClick={() => setShow((prev) => !prev)}
+              />
+            ) : (
+              <IoEye
+                className="absolute w-5 h-5 cursor-pointer right-[5%] bottom-[10%]"
+                onClick={() => setShow((prev) => !prev)}
+              />
+            )}
+          </div>
+
+          <button className="w-[80%] h-10 bg-black text-white cursor-pointer flex items-center justify-center rounded-[5px]">
+            Login
+          </button>
+          <span
+            className="text-[13px] cursor-pointer text-[#585757]"
+            onClick={() => navigate("/forget")}
+          >
+            Forget your password ?
+          </span>
+          <div className="w-[80%] flex items-center gap-2">
+            <div className="w-[25%] h-[0.5px] bg-[#c4c4c4]"></div>
+            <div className="w-[50%] text-[15px] text-[#6f6f6f] flex items-center justify-center">
+              Or continue
+            </div>
+            <div className="w-[25%] h-[0.5px] bg-[#c4c4c4]"></div>
+          </div>
+          <div className="w-[80%] h-10 border border-[black] rounded-[5px] flex items-center justify-center">
+            <img src={google} className="w-6.25" alt="" />
+            <span className="text-[18px] text-gray-500">oogle</span>
+          </div>
+          <div className="text-[#6f6f6f]">
+            Create new account
+            <span
+              className="underline underline-offset-1 text-[black]"
+              onClick={() => navigate("/signup")}
+            >
+              SignUp
+            </span>
+          </div>
+        </div>
+
+        {/* right div */}
+
+        <div className="w-[50%] h-full rounded-r-2xl bg-[black] md:flex items-center justify-center flex-col hidden">
+          <img src={logo} alt="logo" className="w-30 shadow-2xl" />
+          <span className="text-2xl text-white">Start Learning</span>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
